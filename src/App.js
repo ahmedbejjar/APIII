3d869102c695562dd95dd871008428a20fdb52a4
ahmed bejjar <abejjar471@gmail.com>
2023-12-29 10:13:32 +0100

    apii

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import UserList from "./Components/UserList";
+import axios from "axios";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Info from "./Components/Info";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  // const [count, setCount] = useState(0);
+
+  console.log(users);
+  
+  const getUsers = async () => {
+    try {
+      const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(res.data);
+  
+    } catch (error) {
+      console.log(error.response.data);
+    }
+  };
+ 
+  useEffect(() => {
+    getUsers();
+    
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<UserList list={users} />} />
+          <Route path="/info/:id" element={<Info/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
